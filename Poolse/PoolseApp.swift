9ae12d427cf116ecd2c803888e3a39d7f5260694
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-09-20 21:24:52 +0900

    Initail commit

---
+++ Poolse/PoolseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolseApp.swift
+//  Poolse
+//
+//  Created by 김정우 on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PoolseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
